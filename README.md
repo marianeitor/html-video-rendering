1cb06cf5634e25601381c2586b8d5efbaf486044
mariano <mariano1colombo@hotmail.com>
2021-06-23 23:12:05 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+
+
+Installing ffmpeg
+
+sudo apt install ffmpeg
+
+https://linuxize.com/post/how-to-install-ffmpeg-on-ubuntu-18-04/
+
+
+Installing Xvfb
+
+sudo apt-get install -y xvfb
+
+
+https://zoomadmin.com/HowToInstall/UbuntuPackage/xvfb
+
+
+
+Install node JS
+
+https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-20-04-es
+
+For starting run npm start.
+
+You can change paths of html animation and output mp4 file in index.js
+
+Background video should be named bg.mp4 and be placed on same folder
--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const { exec } = require('child_process');
 
+const outputVideoPath = "output.mp4"
+const htmlAnimation = "template.html"
 
 //Starting xvfb process
-exec("Xvfb :1 -screen 0 1280x720x24+32", (error, stdout, stderr) => {});
+const proc1 = exec("Xvfb :1 -screen 0 1280x720x24+32", (error, stdout, stderr) => {
+    console.log(error)
+});
 
 //Starting firefox and pointing to display 1
-exec("DISPLAY=:1 firefox --kiosk template.html", (error, stdout, stderr) => {})
+const proc2 = exec(`DISPLAY=:1 firefox --kiosk ${htmlAnimation}`, (error, stdout, stderr) => {
+    console.log(error)
+})
 
 //Starting ffmpeg to capture
-exec("ffmpeg -video_size `DISPLAY=:1 xdpyinfo | grep 'dimensions:'|awk '{print $2}'` -draw_mouse 0 -framerate 30 -f x11grab -i :1.0+0,0 -c:v libx264rgb -crf 0 -preset ultrafast output.mp4", (error, stdout, stderr) => {})
+const proc3 = exec(`ffmpeg -video_size 1152x648 -draw_mouse 0 -framerate 30 -f x11grab -i :1.0+0,0 -c:v libx264rgb -crf 0 -preset ultrafast ${outputVideoPath}`, (error, stdout, stderr) => {
+    console.log(error)
+})
 
+process.on('SIGINT', function() {
+    console.log("Caught interrupt signal");
+    proc1.kill('SIGINT');
+    proc2.kill('SIGINT');
+    proc3.kill('SIGINT');
+    process.exit();
+});
 
-
-
-
-exec()
